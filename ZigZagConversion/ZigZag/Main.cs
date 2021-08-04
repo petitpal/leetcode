a13a4165f56d83238e83d134b42eb665f62a77f2
Paul Newman <petitpal@yahoo.com>
2021-08-04 09:01:29 +0100

    Adding initial solution

---
+++ ZigZagConversion/ZigZag/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+public class Program {
+
+    public static void Main(string[] args)
+    {
+        var solution = new Solution();
+        var test = solution.Convert("PAYPALISHIRING",4);
+
+    }
+    
+}
