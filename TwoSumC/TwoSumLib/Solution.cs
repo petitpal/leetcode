53ddf5a08e61de178c4a4bb29c5e51b326f4e4c1
Paul Newman <petitpal@yahoo.com>
2021-08-07 08:15:22 +0100

    Adding first stab solution

--- TwoSumC/TwoSumLib/Solution.cs
+++ TwoSumC/TwoSumLib/Solution.cs
@@ -3,8 +3,24 @@
 namespace TwoSumLib
 {
     public class Solution {
+
+
+        // O(n2) solution
         public int[] TwoSum(int[] nums, int target) {
-            return null;
+
+            // O(n^2)
+            for (var i = 0; i < nums.Length; i++) {             // n x (n x O(1))
+                for (var j = 0; j < nums.Length; j++) {         // n x O(1)
+                    if (i != j) {
+                        if (nums[i] + nums[j] == target) {      // O(1)
+                            return new int[] { i, j };
+                        }
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("No solution found");
         }
+
     }
 }
--- TwoSumC/TwoSumTests/UnitTest1.cs
+++ TwoSumC/TwoSumTests/UnitTest1.cs
@@ -15,8 +15,44 @@ namespace TwoSumTests
         [Test]
         public void Test1()
         {
-            var thing = _solution.TwoSum(null, 0);
-            Assert.Pass();
+            TestRunner(
+                new int[] { 2, 7, 11, 15 },
+                9,
+                new int[] { 0, 1 }
+            );
         }
+
+        [Test]
+        public void Test2()
+        {
+            TestRunner(
+                new int[] { 3, 2, 4 },
+                6,
+                new int[] { 1, 2 }
+            );
+        }
+
+        [Test]
+        public void Test3()
+        {
+            TestRunner(
+                new int[] { 3, 3 },
+                6,
+                new int[] { 0, 1 }
+            );
+        }
+
+        public void TestRunner(int[] input, int target, int[] expected)
+        {
+            // arrange
+            
+            // act
+            var output = _solution.TwoSum(input, target);
+
+            // assert
+            Assert.AreEqual(expected[0], output[0]);
+            Assert.AreEqual(expected[1], output[1]);
+        }
+        
     }
 }
